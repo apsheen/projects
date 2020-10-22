6442a30e5c66c419d5e40660c2732dabf969a996
ainsley <ainsleysheen@gmail.com>
2020-10-21 17:17:23 -0700

    rearranging

--- finance/public/history.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // configuration
-    require("../includes/config.php");
-
-    $history = CS50::query("SELECT action, symbol, shares, price, time FROM history WHERE user_id = ?", $_SESSION["id"]);
-    render("history.php", ["title" => "History", "history" => $history]);
-
-?>
--- finance/views/quote.php
+++
@@ -1,3 +0,0 @@
-<div id="middle">
-The stock price for <?=$stock["name"]?> (<?=$_POST["symbol"]?>) is $<?=number_format($stock["price"], 2)?>
-</div>
